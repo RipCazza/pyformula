3c2e4f710111eba4bdd7d4f6c16a54322ee4a244
RipCazza <s.bakker777@gmail.com>
2014-06-21 18:54:23 +0200

    Proof of concept pythagoras formula

---
+++ pyformula/formulae/maths.py
@@ -0,0 +1,20 @@
+from sympy import *
+
+def pythagoras(a=None, b=None, c=None):
+    """a^2 = b^2 + c^2"""
+    arguments = locals()
+    
+    _a, _b, _c = symbols('a b c')
+    symbol_dict = {'a':_a, 'b':_b, 'c':_c}
+    
+    expr = Eq(_a**2, _b**2 + _c**2)
+    
+    for key, value in arguments.iteritems():
+        if value is not None:
+	    expr = expr.subs(symbol_dict[key], value)
+	    
+    return [result.evalf() for result in solve(expr)]
+
+print(pythagoras(b=2, c=4))
+   
+    
