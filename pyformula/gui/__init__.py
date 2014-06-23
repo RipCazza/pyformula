379256440d9f45039e996ce26d37c50b2c88d3f1
Rubykuby <rubykuby@gmail.com>
2014-06-23 15:45:44 +0200

    Added small documentation docstrings.
    Concerns pyformula.gui.get_ui_path() and pyformula.gui.load_ui().

--- pyformula/gui/__init__.py
+++ pyformula/gui/__init__.py
@@ -10,10 +10,14 @@ except ImportError:
     raise pyformula.PySideImportError
 
 def get_ui_path(py_filename):
+    """Transforms a "filename.py" string into a "/path/to/filename.ui" string,
+    where the path is predefined in pyformula.paths.
+    """
     ui_basename = "{}.ui".format(splitext(basename(py_filename))[0])
     return join(pyformula.paths['designer'], ui_basename)
 
 def load_ui(ui_filename):
+    """Returns an instance of a UI widget that is loaded in from a .ui file."""
     ui_file = QFile(ui_filename)
     loader = QUiLoader()
     ui_file.open(QFile.ReadOnly)
