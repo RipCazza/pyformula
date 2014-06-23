0d7028af61f9766ee00085bd3d2c2fe3b544a469
Rubykuby <rubykuby@gmail.com>
2014-06-23 14:37:15 +0200

    Python 2.7 compatibility added by making super() more verbose

--- pyformula/gui/MainWindow.py
+++ pyformula/gui/MainWindow.py
@@ -10,5 +10,5 @@ except ImportError:
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super(MainWindow, self).__init__(parent)
         self.ui = pyformula.gui.loadUi(os.path.basename(__file__))
