058f559d71b4ca73930bd642e0d240c98c320ebb
RipCazza <s.bakker777@gmail.com>
2014-06-23 22:15:32 +0200

    Added sine_law()

--- pyformula/formulae/maths.py
+++ pyformula/formulae/maths.py
@@ -11,4 +11,15 @@ def pythagoras(a=None, b=None, c=None):
     expr = Eq(_a**2 + _b**2, _c**2)
 
     return pyformula.formulae.get_results(expr, arguments, symbol_dict)
-    
+
+def sine_law(a=None, b=None, alpha=None, beta=None):
+    """a/sin(alpha) = b/sin(beta)"""
+    arguments = locals()
+
+    _a, _b, _alpha, _beta = symbols('a b alpha beta')
+    symbol_dict = {'a':_a, 'b':_b, 'alpha':_alpha, 'beta':_beta}
+
+    expr = Eq(_a/sin(_alpha), _b/sin(_beta))
+
+    return pyformula.formulae.get_results(expr, arguments, symbol_dict)
+
