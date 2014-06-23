5bb34fc958b3548860ecab4ebe6d163a1fcdfbbf
Rubykuby <rubykuby@gmail.com>
2014-06-23 15:33:14 +0200

    Implemented get_ui_path(). Normalised function names

--- pyformula/gui/MainWindow.py
+++ pyformula/gui/MainWindow.py
@@ -1,5 +1,4 @@
 import pyformula
-import os
 try:
     from PySide import QtCore, QtGui
     from PySide.QtCore import Signal, Slot, QFile
@@ -11,4 +10,4 @@ except ImportError:
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        self.ui = pyformula.gui.loadUi(os.path.basename(__file__))
+        self.ui = pyformula.gui.load_ui(pyformula.gui.get_ui_path(__file__))
--- pyformula/gui/__init__.py
+++ pyformula/gui/__init__.py
@@ -1,4 +1,5 @@
 import os
+from os.path import basename, splitext, join
 import pyformula
 from .MainWindow import MainWindow
 try:
@@ -8,9 +9,12 @@ try:
 except ImportError:
     raise pyformula.PySideImportError
 
-def loadUi(py_filename):
-    ui_file = QFile(os.path.join(pyformula.paths['designer'],
-        "{}.ui".format(py_filename[:-len(".py")])))
+def get_ui_path(py_filename):
+    ui_basename = "{}.ui".format(splitext(basename(py_filename))[0])
+    return join(pyformula.paths['designer'], ui_basename)
+
+def load_ui(ui_filename):
+    ui_file = QFile(ui_filename)
     loader = QUiLoader()
     ui_file.open(QFile.ReadOnly)
     ui = loader.load(ui_file)
