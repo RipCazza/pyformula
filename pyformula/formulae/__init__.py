a8fa123e714aaa0b3a02db10fcd069b6779c75d2
Rubykuby <rubykuby@gmail.com>
2014-06-24 16:50:41 +0200

    Added back docstring to get_results()

--- pyformula/formulae/__init__.py
+++ pyformula/formulae/__init__.py
@@ -3,6 +3,17 @@ import pyformula
 
 
 def get_results(expr, values, symbols_):
+    """Iterate over the number/None values of `values`. For every value that is
+    not None, perform a substitution.
+
+    `expr` holds references to objects that `symbols_` stores. The corresponding
+    substitution value of these objects is stored in the same indices in
+    `values` as they do in `symbols_`. Using these common indices, the objects
+    in `expr` are replaced with values from `values`.
+
+    After the substitution, the expression is solved and the floats of the
+    solved expression are returned in a list.
+    """
     for i, value in enumerate(values):
         if value is not None:
             expr = expr.subs(symbols_[i], value)
