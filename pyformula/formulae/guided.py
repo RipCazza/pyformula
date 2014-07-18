dba2bdbee216660c96e42a37eebc71fcfa22cd45
JRZCaze <jessezwitserlood@gmail.com>
2014-07-18 22:40:15 +0200

    Refactored a tiny bit of instructions

--- pyformula/formulae/guided.py
+++ pyformula/formulae/guided.py
@@ -1,26 +1,40 @@
 import math
 
+
+class Instruction(object):
+
+    def __init__(self, orig_func, filled_in_func, ans):
+        self.orig_func = orig_func
+        self.filled_in_func = filled_in_func
+        self.ans = ans
+
+
 def abc(a, b, c):
     instructions = []
-    instructions.append("ax^2 + bx + c = 0")
-    instructions.append("{a}x^2 + {b}x + {c} = 0".format(a=a, b=b, c=c))
+    instructions.append(Instruction("0 = ax^2 + bx + c",
+                                    "{a}x^2 + {b}x + {c}".format(a=a, b=b, c=c),
+                                    "0"))
 
     D = b**2 - 4 * a * c
-    instructions.append("D = b^2 - 4 * a * c")
-    instructions.append("{D} = {b}^2 - 4 * {a} * {c}".format(a=a, b=b, c=c,
-                                                             D=D))
+    instructions.append(Instruction("D = b^2 - 4 * a * c",
+                                    "{b}^2 - 4 * {a} * {c}".format(a=a, b=b,
+                                                                   c=c),
+                                    str(D)))
 
     if D < 0:
         raise Exception()
 
     x1 = (-b + math.sqrt(D)) / (2 * a)
     x2 = (-b - math.sqrt(D)) / (2 * a)
-    instructions.append("x = (-b +/- sqrt(D)) / (2 * a)")
-    instructions.append("(-{b} +/- sqrt({D})) / (2 * {a})".format(b=b, D=D,
-                                                                  a=a))
-    instructions.append("x = {x1} V x = {x2}".format(x1=x1, x2=x2))
 
-    return [x1, x2], instructions
+    instructions.append(Instruction("x1 = (-b + sqrt(D)) / (2 * a) V "
+                                    "x2 = (-b - sqrt(D)) / (2 * a)",
+                                    "(-{b} + sqrt({D})) / (2 * {a}) V "
+                                    "(-{b} - sqrt({D})) / (2 * {a})".format(
+                                        b=b, D=D, a=a),
+                                    "{x1} V {x2}".format(x1=x1, x2=x2)))
+
+    return instructions
 
 def exponential_sum(a, p, q):
     instructions = []
